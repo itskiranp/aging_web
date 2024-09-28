4af352f8bc134318c48691d9b3d064ae48dbe4ad
e99sy <kiranpou950@gmail.com>
2024-09-28 17:37:35 +0545

    layout page styling and some css code removed

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
-    <link href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.4/main.min.css" rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -31,18 +30,7 @@
                                 <i class="fa fa-brands fa-linkedin "></i>
                             </a>
                         </div>
-
-
-                        {{-- <nav>
-                                <ul class="nav ">
-                                    <li class="nav-item">
-                                        <a class="nav-link text-dark" href="#"><strong>Global</strong></a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link text-dark" href="#"><strong>Regions</strong></a>
-                                    </li>
-                                </ul>
-                            </nav> --}}
+                        
                     </div>
                 </div>
 
@@ -71,30 +59,25 @@
                 </div>
             </div>
         </div>
-
         <div id="sf-main-header" class="main-header">
-            <div class="row">
+            <div class="row ">
                 <!-- Logo Section -->
-                <div class="col-md-2">
-                    <div class="header-logo logos-container ">
+                <div class="col-md-2 col-4">
+                    <div class="header-logo logos-container">
                         <a href="/" title="Home" class="primary-logo">
-
-                            <img src="/images/aging_logo.png" class="logo "></img>
+                            <img src="/images/aging_logo.png" class="logo"></img>
                         </a>
                     </div>
                 </div>
-                <div class="col-md-9 ">
-                    <div class="header-text text">
-                        <p class="h1 mt-3 mb-0" style="color:#00477d"><strong>Chitwan Valley Family Study (CVFS)</strong></p>
-                        <p class="h2"style="color:#00477d">Study on Cognition
-                            and Aging in Nepal</p>
-                        {{-- <h2 class="mt-3" style="color:#00477d"><strong>Chitwan Valley Family Study - Study on Cognition
-                            and Aging in Nepal</strong></h2> --}}
+                <div class="col-md-9 col-8">
+                    <div class="header-text align-item-start">
+                        <p class="h1 mt-3 mb-0" style="color:rgb(54, 54, 252)"><strong>Chitwan Valley Family Study (CVFS)</strong></p>
+                        <p class="h2" style="color: rgb(54, 54, 252)">Study on Cognition and Aging in Nepal</p>
                     </div>
-
                 </div>
             </div>
         </div>
+        
         <nav class="navbar navbar-expand-lg ">
             <div class="container ">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -165,9 +148,6 @@
                 </div>
             </div>
         </nav>
-
-
-
     </header>
 
     <!-- Main Content Section -->
@@ -187,14 +167,7 @@
                 <p><i class="fa fa-phone" aria-hidden="true"></i> +977-56-591054, +977-56-592406</p>
                 <p><i class="fa fa-envelope" aria-hidden="true"></i> iser.nepal@outlook.com</p>
                 <p><i class="fa fa-clock" aria-hidden="true"></i> 10 A.M. - 5 P.M.</p>
-
-                {{-- <div class="social-icons">
-                    <a href="https://www.facebook.com/isernepalofficial/" target="_blank">
-                        <i class="fa fa-brands fa-facebook fa-2x"></i></a>
-                    <a href="https://www.linkedin.com/in/iser-nepal-68b407160/" target="_blank">
-                        <i class="fa fa-brands fa-linkedin fa-2x"></i>
-                    </a>
-                </div> --}}
+               
             </div>
             <!-- Logo and Research Project Section -->
             <div class="footer-right">
