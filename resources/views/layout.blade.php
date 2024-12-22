876b6a57f1c8fa5fb6883b7541b6d2f900088ced
e99sy <kiranpou950@gmail.com>
2024-12-22 13:25:32 +0545

    updated

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -60,7 +60,7 @@
                 <!-- Logo Section -->
                 <div class="col-md-6">
                     <a href="{{ url('/') }}" class="logo"  >
-                        <img src="/images/header_logo_updated.PNG" class="img-fluid" alt="...">
+                        <img src="/images/header_logo_photoshop.png" class="img-fluid" alt="...">
                     </a>
                 </div>
             </div>
