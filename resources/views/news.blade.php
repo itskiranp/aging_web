e9155ffa28513bc36b9616b5d89168478555ae59
itskiranp <kiranpou950@gmail.com>
2024-12-31 22:16:06 +0545

    updated images in news and events page

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -20,6 +20,30 @@
           <h1 class="card-title">{{ $news->title }}</h1>
           <p class="card-text text-muted small">{{ $news->published_at->format('M d, Y') }}</p>
           <p class="card-text">{!! $news->content !!}</p>
+          <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="/images/cvfs2024-1.jpeg" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="/images/cvfs2024-2.jpeg" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="/images/cvfs2024-3.jpeg" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="/images/cvfs2024-5.jpeg" class="d-block w-100" alt="...">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
       </div>
   </div>
 
