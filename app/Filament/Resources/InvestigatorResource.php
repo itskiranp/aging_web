cc27d171afc2fd50341e61d36347e6145118b258
itskiranp <kiranpou950@gmail.com>
2025-09-04 10:38:57 +0545

    updated filament file code

--- app/Filament/Resources/InvestigatorResource.php
+++ app/Filament/Resources/InvestigatorResource.php
@@ -7,9 +7,7 @@ use App\Filament\Resources\InvestigatorResource\RelationManagers;
 use App\Models\Investigator;
 use Filament\Forms;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\MarkdownEditor;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Forms\Form;
@@ -17,9 +15,6 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Actions\DeleteBulkAction;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Mohamedsabil83\FilamentFormsTinyeditor\Components\TinyEditor;
 
 class InvestigatorResource extends Resource
@@ -32,39 +27,51 @@ class InvestigatorResource extends Resource
     {
         return $form
             ->schema([
-                //
                 TextInput::make('name')->required(),
                 TextInput::make('position')->required(),
                 TextInput::make('phone')->required(),
                 TextInput::make('email')->email()->required(),
-                TinyEditor::make('bio')->nullable(),
-                TinyEditor::make('description')
-                    ->columnSpanFull()
+
+                TinyEditor::make('bio')
                     ->profile('default')
-                    ->label('Description')
-                    ->required()
-                    ->columnSpanFull()
-                    ->toolbar('undo redo | bold italic underline | alignleft aligncenter alignright | bullist numlist outdent indent | removeformat')
-                    ->plugins('paste')
+                    ->label('Bio')
+                    ->nullable()
                     ->options([
-                        'paste_as_text' => true, // forces paste as plain text
+                        'menubar' => false,
+                        'plugins' => 'paste lists link',
+                        'toolbar' => 'undo redo | bold italic underline | bullist numlist | alignleft aligncenter alignright | removeformat',
+                        'paste_as_text' => true,
                         'paste_auto_cleanup_on_paste' => true,
                         'paste_remove_styles' => true,
                         'paste_remove_spans' => true,
                         'paste_strip_class_attributes' => 'all',
-                    ]),  
+                    ]),
+
+                TinyEditor::make('description')
+                    ->profile('default')
+                    ->label('Description')
+                    ->required()
+                    ->columnSpanFull()
+                    ->options([
+                        'menubar' => false,
+                        'plugins' => 'paste lists link',
+                        'toolbar' => 'undo redo | bold italic underline | bullist numlist | alignleft aligncenter alignright | removeformat',
+                        'paste_as_text' => true,
+                        'paste_auto_cleanup_on_paste' => true,
+                        'paste_remove_styles' => true,
+                        'paste_remove_spans' => true,
+                        'paste_strip_class_attributes' => 'all',
+                    ]),
+
                 FileUpload::make('image')
                     ->directory('images')
                     ->label('Image')
                     ->preserveFilenames()
                     ->nullable(),
 
-
-
                 FileUpload::make('profile_pdf')
                     ->directory('uploads') // This ensures CVs are uploaded to /public/uploads
                     ->label('Curriculum Vitae'),
-
             ]);
     }
 
@@ -72,13 +79,12 @@ class InvestigatorResource extends Resource
     {
         return $table
             ->columns([
-
                 TextColumn::make('name')->sortable()->searchable(),
                 TextColumn::make('position'),
                 TextColumn::make('email'),
                 TextColumn::make('phone'),
                 ImageColumn::make('image')->label('Profile Pic')->circular(),
-                TextColumn::make('created_at')->since()
+                TextColumn::make('created_at')->since(),
             ])
             ->filters([
                 //
